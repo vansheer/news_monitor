7765c6245dedae11c8c8ed04761970413a023b38
Vansheer <goovan@gmail.com>
2021-06-19 12:34:45 -0400

    specify english

--- news_monitor.py
+++ news_monitor.py
@@ -7,7 +7,7 @@ newsapi = NewsApiClient(api_key=st.secrets['api_key'])
 
 search_term = st.text_input('输入关键词', max_chars=15)
 if search_term:
-    data = newsapi.get_everything(q=search_term)
+    data = newsapi.get_everything(q=search_term, language='en')
     df = pd.DataFrame(data['articles'])
     for col in df.select_dtypes(include='object'):
         df[col] = df[col].str.strip()
