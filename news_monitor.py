9476adf424ec3c9dff162bcb2281a983727bfd43
Vansheer <goovan@gmail.com>
2021-06-19 12:22:18 -0400

    first commit

---
+++ news_monitor.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+from newsapi import NewsApiClient
+import datetime
+
+newsapi = NewsApiClient(api_key='761ac61282ca4aefb3fb755ed9c55a89')
+
+search_term = st.text_input('输入关键词', max_chars=15)
+if search_term:
+    data = newsapi.get_everything(q=search_term)
+    df = pd.DataFrame(data['articles'])
+    for col in df.select_dtypes(include='object'):
+        df[col] = df[col].str.strip()
+    df.drop_duplicates(subset='title', inplace=True)
+    for news in data['articles'][:5]:
+        st.write(f"来源：{news['source']['name']}")
+        st.write(f"标题：{news['title']}")
+        st.write(f"作者：{news['author']}")
+        st.write(f"正文：{news['description']}")
+        st.write(f"原文链接：{news['url']}")
+        st.write('\n')
